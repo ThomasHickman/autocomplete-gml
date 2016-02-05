47fa4d1d49082530014173acd91ec3feb3c87734
ThomasHickman <Thomas.Hickman42@gmail.com>
2016-02-05 19:40:57 +0000

    Added variable autocomplete

--- lib/main.ts
+++ lib/main.ts
@@ -3,33 +3,54 @@
 import fs = require("fs");
 import path = require("path");
 
-interface ObjectDescriptor{
+interface FunctionDescriptor{
     name: string;
     parameters: string[]
 }
 
+interface FunctionVariable{
+    name: string;
+}
+
+interface AutoCompleteData{
+    text: string;
+    type: string;
+    rightLabel?: string;
+}
 
 class Provider{
-    private autocompleteFunctions: ObjectDescriptor[];
+    private autocompleteFunctions: FunctionDescriptor[];
+    private autocompleteVariables: FunctionVariable[];
     selector = '.source.gml';
     //disableForSelector: '.source.js .comment',
     inclusionPriority = 1;
     excludeLowerPriority = false;
-    getFunctionsWithPrefix(prefix: string){
-        return this.autocompleteFunctions.filter(el => {
+    getSymbolFromPrefix<t extends {name: string}[]>(prefix: string, list: t){
+        return <t>list.filter(el => {
             return el.name.slice(0, prefix.length) == prefix;
         })
+    }
+    getFunctionsWithPrefix(prefix: string){
+        return this.getSymbolFromPrefix(prefix, this.autocompleteFunctions)
+    }
+    getVariablesWithPrefix(prefix: string){
+        return this.getSymbolFromPrefix(prefix, this.autocompleteVariables)
     }
     getSuggestions(arg) {
         var activatedManually: boolean, bufferPosition: number, editor, prefix: string, scopeDescriptor;
         editor = arg.editor, bufferPosition = arg.bufferPosition, scopeDescriptor = arg.scopeDescriptor, prefix = arg.prefix, activatedManually = arg.activatedManually;
         console.log(prefix);
         return new Promise((resolve) => {
-            return resolve(this.getFunctionsWithPrefix(prefix).map(obDesc => ({
+            console.time("concat")
+            var retValue = resolve((<AutoCompleteData[]>this.getFunctionsWithPrefix(prefix).map(obDesc => ({
                 text: obDesc.name,
                 rightLabel: obDesc.parameters != null?"(" + obDesc.parameters.join(", ") + ")":"()",
                 type: "function"
-            })));
+            }))).concat(this.getVariablesWithPrefix(prefix).map(obDesc => ({
+                text: obDesc.name,
+                type: "variable"
+            }))));
+            console.timeEnd("concat")
         });
     }
     onDidInsertSuggestion(arg) {
@@ -38,7 +59,7 @@ class Provider{
     }
     dispose(){}
     constructor(){
-        fs.readFile(path.resolve(__dirname, "..", "gml-data"), (err, data) => {
+        fs.readFile(path.resolve(__dirname, "..", "gml-functions"), (err, data) => {
             var functionDescriptors = data.toString().split("--")[1]/*Remove the copywrite*/.split(/(\r\n|\n|\r)\1/);
             this.autocompleteFunctions = functionDescriptors.map(functionDescriptor => {
                 var parts = functionDescriptor.split(/\r\n|\n|\r/);
@@ -56,6 +77,11 @@ class Provider{
                 }
             })
         })
+        fs.readFile(path.resolve(__dirname, "..", "gml-variables"), (err, data) => {
+            this.autocompleteVariables = data.toString()
+                                             .split(/\r\n|\n|\r/)
+                                             .map(x => ({name: x}));
+        });
     }
 };
 
